60e0b9d1f0f0c37259c9965d6f4ee09e35004c0a
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-03-09 14:49:53 +0700

    initial

---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  desc: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+	<div class="card">
+		<div>{{ desc }}</div>
+	</div>
+</template>
